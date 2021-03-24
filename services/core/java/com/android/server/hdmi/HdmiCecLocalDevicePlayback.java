ede99170d56ced425ec390df66ed791ceab1d9ac
bross1_kuo <bross1_kuo@asus.com>
2021-03-24 14:43:47 +0800

    HDMI: CEC: set mAutoTvOff default value to true
    for trigger TV enter standby mode when device display off
    
    Change-Id: Idca10301cd12000e3fe7358760baddb3641eb9df
    Signed-off-by: bross1_kuo <bross1_kuo@asus.com>

--- services/core/java/com/android/server/hdmi/HdmiCecLocalDevicePlayback.java
+++ services/core/java/com/android/server/hdmi/HdmiCecLocalDevicePlayback.java
@@ -68,7 +68,7 @@ public class HdmiCecLocalDevicePlayback extends HdmiCecLocalDeviceSource {
     HdmiCecLocalDevicePlayback(HdmiControlService service) {
         super(service, HdmiDeviceInfo.DEVICE_PLAYBACK);
 
-        mAutoTvOff = mService.readBooleanSetting(Global.HDMI_CONTROL_AUTO_DEVICE_OFF_ENABLED, false);
+        mAutoTvOff = mService.readBooleanSetting(Global.HDMI_CONTROL_AUTO_DEVICE_OFF_ENABLED, true);
 
         // The option is false by default. Update settings db as well to have the right
         // initial setting on UI.
